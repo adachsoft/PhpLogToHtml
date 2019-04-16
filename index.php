ed0d8af119e0788b19515332231d1fe593609c23
adachsoft <adach_soft@wp4b.pl>
2019-04-16 05:54:41 +0200

    Change of appearance

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php 
-    $urlWebSocket = "ws://" . $_SERVER['SERVER_ADDR'] . ":8080/";
+    if( 'cli-server'===php_sapi_name() ){
+        [$ip, $port] = explode(':', $_SERVER['HTTP_HOST']);
+    }else{
+        $ip = $_SERVER['SERVER_ADDR'];
+    }
+    $urlWebSocket = "ws://{$ip}:8080/";
+
+    /*var_dump(php_sapi_name());
+
+    $host= gethostname();
+$ip = gethostbyname($host);
+var_dump($host, $ip, $_SERVER);*/
+
 ?><!DOCTYPE HTML>
 <html>
     <head>
@@ -8,39 +20,56 @@
             var cnt = 0;
             function runWebSocket() {
                 if ("WebSocket" in window) {
-                    console.log("WebSocket is supported by your Browser!");
-                    // Let us open a web socket
                     var ws = new WebSocket("<?php echo $urlWebSocket; ?>");
-                    ws.onopen = function() {
-                        
-                        // Web Socket is connected, send data using send()
-                        ws.send("Message to send");
-                        console.log("Message is sent...");
+                    ws.onopen = () => {
+
                     };
-                    ws.onmessage = function (evt) { 
-                        var receivedMsg = evt.data;
-                        let color = cnt % 2 === 0 ? '#e0e0e0' : '#d0d0d0';
-                        $('#result').html($('#result').html() + "<pre style=\"background-color: " + color + "\">" + receivedMsg + "</pre>");
+                    ws.onmessage = (evt) => { 
+                        let receivedMsg = evt.data;
+                        //let color = cnt % 2 === 0 ? '#e0e0e0' : '#d0d0d0';
+                        //$('#result').html($('#result').html() + "<pre style=\"background-color: " + color + "\">" + receivedMsg + "</pre>");
+                        $('#result').html($('#result').html() + "<pre>" + receivedMsg + "</pre>");
                         cnt++;
-                        $('html, body').animate({scrollTop:$(document).height()}, 'slow');
+                        $('html, body').animate({scrollTop:$(document).height()}, 'fast');
                     };
-                    ws.onclose = function() { 
-                        // websocket is closed.
-                        console.log("Connection is closed..."); 
+                    ws.onclose = () => { 
+                        
                     };
                 } else {
-                    // The browser doesn't support WebSocket
                     console.log("WebSocket NOT supported by your Browser!");
                 }
+            }
+            function clearAll(){
+                $('#result').html('');
             }
             $(document).ready(()=>{
                 runWebSocket();
             });
         </script>
+        <style>
+            pre{
+                padding: 5px;
+                color: #808080;
+                border-top: 1px #ffffff dotted;
+            }
+            div#result{
+                padding: 5px;
+                background-color: black;
+            }
+            div#header{
+                position: fixed;
+                top: 0;
+                left: 0;
+                right: 0;
+                height: 50px;
+                background-color: #3399ff;
+            }
+        </style>
     </head>
     <body>
-        <?php echo $urlWebSocket; ?><br>
-        <div id="result">
+        <div id="header">
+            <?php echo $urlWebSocket; ?> <button type="button" onclick="clearAll()">Clear all</button>
         </div>
+        <div id="result"></div>
     </body>
 </html>
